6754c2cfe9c724b9598f689dfcb2d7a11bf492ab
Shintaro Kurosawa <bonjin6770@yahoo.co.jp>
2015-02-20 09:56:18 +0900

    new solution

---
+++ TryCSCookbook3rdEditionSolution/Recipes1.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Messaging;
+
+namespace Recipes1._1 {
+    class Program {
+        static void Main(string[] args) {
+            // 既存のメッセージキューを開きます。
+            string queuePath = @".\private$\LINQMQ";
+            MessageQueue messageQueue = new MessageQueue(queuePath);
+            BinaryMessageFormatter messageFormatter = new BinaryMessageFormatter();
+
+            var query = from Message msg in messageQueue
+                        // 以下の１行目でmsg.Formatterを指定することにより、
+                        // Messageオブジェクトを操作できるようになります。
+                        // ここでは各メッセージのインスタンスについてBinaryMessageFormatterを
+                        // 指定することにより、指定した条件に一致するかどうか確認しています。
+                        // その後指定したFormatterが正しく設定されたかどうかを等号により
+                        // 確認しています。Where句ではboolean型の結果を返す文を
+                        // 指定する必要がありますが、このようにすることでFormatterを
+                        // Where句の中で指定できます。
+                        where ((msg.Formatter = messageFormatter) == messageFormatter) &&
+                        int.Parse(msg.Label) > 5 &&
+                        msg.Body.ToString().Contains('D')
+                        orderby msg.Body.ToString() descending
+                        select msg;
+
+            foreach (var msg in query) {
+                Console.WriteLine("lable: " + msg.Label + " Body: " + msg.Body);
+            }
+
+        }
+    }
+}
---
+++ TryCSCookbook3rdEditionSolution/TryCSCookbook3rdEdition/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TryCSCookbook3rdEdition {
+    class Program {
+        static void Main(string[] args) {
+            // Recipes1.1 
+
+        }
+    }
+}
